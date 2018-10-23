7b7e0c666da9e5685ce394ea00058a3950997c98
soleilyasmina <mounlasolomon@gmail.com>
2018-10-23 16:15:48 -0400

    reset files

--- js/app.js
+++ js/app.js
@@ -60,74 +60,6 @@ const createGameReds = () => {
 }
 
 /**
- * [createViewBlacks description]
- * @return {[type]} [description]
- */
-const createViewBlacks = () => {
-  for (let i = 1; i <= 15; i++) {
-    let black = document.createElement('div');
-    black.className = 'black-piece';
-    black.dataset.number = i;
-    black.id = 'black' + i;
-    black.addEventListener('click', () => console.log(black.dataset.number));
-    if (i < 3) {
-      black.style.top = 200 + (40*(i-1)) + 'px';
-      black.style.left = '20px';
-      document.querySelector('#top-left').appendChild(black);
-    }
-    else if (i < 8) {
-      black.style.top = 200 + (40*(i-3)) + 'px';
-      black.style.left = '420px';
-      document.querySelector('#top-right').appendChild(black);
-    }
-    else if (i < 11) {
-      black.style.top = 595 - (40*(i-8)) + 'px';
-      black.style.left = '100px';
-      document.querySelector('#top-right').appendChild(black);
-    }
-    else {
-      black.style.top = 595 - (40*(i-11)) + 'px';
-      black.style.left = '420px';
-      document.querySelector('#top-left').appendChild(black);
-    }
-  }
-}
-
-/**
- * [createViewReds description]
- * @return {[type]} [description]
- */
-const createViewReds = () => {
-  for (let i = 1; i <= 15; i++) {
-    let red = document.createElement('div');
-    red.className = 'red-piece';
-    red.dataset.number = i;
-    red.id = 'red' + i;
-    red.addEventListener('click', () => console.log(red.dataset.number));
-    if (i < 3) {
-      red.style.top = 595 - (40*(i-1)) + 'px';
-      red.style.left = '20px';
-      document.querySelector('#top-left').appendChild(red);
-    }
-    else if (i < 8) {
-      red.style.top = 595 - (40*(i-3)) + 'px';
-      red.style.left = '420px';
-      document.querySelector('#top-right').appendChild(red);
-    }
-    else if (i < 11) {
-      red.style.top = 200 + (40*(i-8)) + 'px';
-      red.style.left = '100px';
-      document.querySelector('#top-right').appendChild(red);
-    }
-    else {
-      red.style.top = 200 + (40*(i-11)) + 'px';
-      red.style.left = '420px';
-      document.querySelector('#top-left').appendChild(red);
-    }
-  }
-}
-
-/**
  * [gameBoard description]
  * @return {[type]} [description]
  */
@@ -143,5 +75,5 @@ const gameBoard = () => {
 gameBoard();
 
 const moveViewPiece = (piece, place) => {
-  
+
 }
